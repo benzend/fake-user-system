528bb40b92b22dc3bcb8297602945f63f03d217a
Benjamin Scott <hteimaj3@gmail.com>
2022-05-20 01:11:50 -0600

    refactor: `Some(MoneyKind::Usd)` hold with `usd`

--- src/main.rs
+++ src/main.rs
@@ -95,12 +95,13 @@ struct Ticket {
 impl Ticket {
     fn new(kind: TicketKind) -> Ticket {
         let id = rand::random::<u16>();
+        let usd = Some(MoneyKind::Usd);
         match kind {
-            TicketKind::DisneyWorld => Ticket { id, cost: Money::new(Some(MoneyKind::Usd), 200), kind},
-            TicketKind::DisneyLand => Ticket { id, cost: Money::new(Some(MoneyKind::Usd), 150), kind},
-            TicketKind::Lagoon => Ticket { id, cost: Money::new(Some(MoneyKind::Usd), 80), kind},
-            TicketKind::ReelTheatre => Ticket { id, cost: Money::new(Some(MoneyKind::Usd), 20), kind},
-            TicketKind::Football => Ticket { id, cost: Money::new(Some(MoneyKind::Usd), 200), kind}
+            TicketKind::DisneyWorld => Ticket { id, cost: Money::new(usd, 200), kind},
+            TicketKind::DisneyLand => Ticket { id, cost: Money::new(usd, 150), kind},
+            TicketKind::Lagoon => Ticket { id, cost: Money::new(usd, 80), kind},
+            TicketKind::ReelTheatre => Ticket { id, cost: Money::new(usd, 20), kind},
+            TicketKind::Football => Ticket { id, cost: Money::new(usd, 200), kind}
         }
     }
 }
