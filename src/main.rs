024a853c3fc4a21e0424ef5db7bd55a356f1c415
Benjamin Scott <hteimaj3@gmail.com>
2022-05-21 15:29:06 -0600

    feat: ticket randomizer added

--- src/main.rs
+++ src/main.rs
@@ -149,3 +149,15 @@ enum JobKind {
     SoftwareEngineer,
     Retail
 }
+
+struct TicketRandomizer {
+    tickets_to_pick_from: Vec<Ticket>,
+}
+
+impl TicketRandomizer {
+    fn randomly_pick_ticket(&self) -> &Ticket {
+        let pick = rand::random::<u8>() % self.tickets_to_pick_from.len() as u8;
+
+        return &self.tickets_to_pick_from[pick as usize];
+    }
+}
