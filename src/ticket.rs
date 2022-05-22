1bf7eebb4188652e5884a1f0066d848244b3b1f8
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-05-21 20:50:56 -0600

    chore: removed `id` prop since not used

--- src/ticket.rs
+++ src/ticket.rs
@@ -2,22 +2,20 @@ use crate::money::Money;
 use crate::money::MoneyKind;
 
 pub struct Ticket {
-  id: u16,
   pub cost: Money,
   pub kind: TicketKind
 }
 
 impl Ticket {
   pub fn new(kind: TicketKind) -> Ticket {
-      let id = rand::random::<u16>();
       let usd = Some(MoneyKind::Usd);
       match kind {
-          TicketKind::DisneyWorld => Ticket { id, cost: Money::new(usd, 200), kind},
-          TicketKind::DisneyLand => Ticket { id, cost: Money::new(usd, 150), kind},
-          TicketKind::Lagoon => Ticket { id, cost: Money::new(usd, 80), kind},
-          TicketKind::ReelTheatre => Ticket { id, cost: Money::new(usd, 20), kind},
-          TicketKind::Football => Ticket { id, cost: Money::new(usd, 200), kind},
-          TicketKind::RoaringSprings => Ticket { id, cost: Money::new(usd, 100), kind}
+          TicketKind::DisneyWorld => Ticket { cost: Money::new(usd, 200), kind},
+          TicketKind::DisneyLand => Ticket { cost: Money::new(usd, 150), kind},
+          TicketKind::Lagoon => Ticket { cost: Money::new(usd, 80), kind},
+          TicketKind::ReelTheatre => Ticket { cost: Money::new(usd, 20), kind},
+          TicketKind::Football => Ticket { cost: Money::new(usd, 200), kind},
+          TicketKind::RoaringSprings => Ticket { cost: Money::new(usd, 100), kind}
       }
   }
 }
